bed6d3b0cb5c89222c01441c28c2586160b95a81
liuhp <liuhp@jackyun.com>
2021-09-14 13:59:22 +0800

    正则表达式demo初始化

---
+++ src/main/java/com/pokors/regular/RegularDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pokors.regular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegularDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegularDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pokors/regular/RegularDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokors.regular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegularDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
